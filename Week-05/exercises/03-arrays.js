cce8c5a9b1901600d0237e46675b4ff7fc1d774a
unknown <olucero921@gmail.com>
2023-04-17 19:00:21 -0300

    BaSP-W05-2023: Arrays

--- Week-05/exercises/03-arrays.js
+++ Week-05/exercises/03-arrays.js
@@ -1,15 +1,72 @@
 console.log('--Ejercicio 3: ARRAYS');
 
-// Ejercicio A
+// Ejercicio a. Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto",
+// "Septiembre", "Octubre", "Noviembre", "Diciembre"] mostrar por consola los meses 5 y 11 (utilizar console.log).
 
-// Ejercicio B
+console.log('-Exercise 3.a:');
 
-// Ejercicio C
+const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto",
+"Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
-// Ejercicio D
+console.log(meses[4]);
+console.log(meses[10]);
 
-// Ejercicio E
+// Ejercicio b. Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).
+console.log('-Exercise 3.b:');
 
-// Ejercicio F
+const mesesB = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
-// Ejercicio G
+mesesB.sort();
+
+console.log(mesesB);
+
+
+// Ejercicio c. Agregar un elemento al principio y al final del array (utilizar unshift y push).
+console.log('-Exercise 3.c:');
+
+const mesesC = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+mesesC.unshift("Principio");
+mesesC.push("Final");
+
+console.log(mesesC);
+
+
+// Ejercicio d. Quitar un elemento del principio y del final del array (utilizar shift y pop).
+console.log('-Exercise 3.d:');
+
+const mesesD = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+mesesD.shift(); 
+mesesD.pop(); 
+
+console.log(mesesD);
+
+// Ejercicio e. Invertir el orden del array (utilizar reverse).
+console.log('-Exercise 3.e:');
+
+const mesesE = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+mesesE.reverse();
+
+console.log(mesesE);
+
+// Ejercicio f.Unir todos los elementos del array en un único string donde cada
+// mes este separado por un guión - (utilizar join).
+console.log('-Exercise 3.f:');
+
+const mesesF = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+const mesesUnidos = mesesF.join("-");
+
+console.log(mesesUnidos);
+
+// Ejercicio g. Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice).
+
+console.log('-Exercise 3.g:');
+
+const mesesG = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+const copiaMeses = mesesG.slice(4, 11);
+
+console.log(copiaMeses);
